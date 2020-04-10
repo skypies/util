2bae0802b791cc483e267e63a73409fb7b28119d
Adam Worrall <adam@worrall.cc>
2020-04-09 17:09:43 -0700

    Small tidyup of template loading

--- handlerware/templates.go
+++ handlerware/templates.go
@@ -2,14 +2,18 @@ package handlerware
 
 import(
 	"errors"
+	"fmt"
 	"html/template"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
 	"time"
 
 	"golang.org/x/net/context"
 
 	"github.com/skypies/util/date"
-	"github.com/skypies/util/widget"
 )
 
 var(
@@ -27,7 +31,7 @@ func InitTemplates(templateDir string) {
 
 // This will PANIC if the dir does not exist or is empty !
 func loadTemplates(dir string) *template.Template {
-	return widget.ParseRecursive(template.New("").Funcs(templateFuncMap()), dir)
+	return ParseRecursive(template.New("").Funcs(templateFuncMap()), dir)
 }
 
 // GetFoo: given a context, extracts the object (or panics; should not be optional). May return nil.
@@ -36,6 +40,48 @@ func GetTemplates(ctx context.Context) (*template.Template) {
 	if (!ok) { panic ("handlerware.GetTemplates: no object found in context") }
 	return tmpl
 }
+
+// ParseRecursive walks the directory structure, loading all the files
+// it finds. Will panic on failure. Follows symlinks.
+func ParseRecursive(t *template.Template, dir string) *template.Template {
+	dirStack := []string{dir}
+	fileStack := []string{}
+
+	for {
+		if len(dirStack) == 0 { break }
+
+		dir := dirStack[0]
+		dirStack = dirStack[1:]
+
+		contents,err := ioutil.ReadDir(dir)
+		if err != nil {
+			str := ""
+			filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+				if err != nil { return err }
+				if info.IsDir() {
+					str += info.Name() + "/\n"
+				}
+				return nil
+			})
+
+			panic(fmt.Errorf("ReadDir: %v\n\nContents of .:\n--\n%s", err, str))
+		}
+
+		for _,f := range contents {
+			if strings.HasPrefix(f.Name(), ".") {
+				continue
+			}
+			if f.IsDir() || (f.Mode() & os.ModeSymlink != 0) {
+				dirStack = append(dirStack, dir+"/"+f.Name())
+			} else {
+				fileStack = append(fileStack, dir+"/"+f.Name())
+			}
+		}
+	}
+
+	return template.Must(t.ParseFiles(fileStack...))
+}
+
 
 
 // Some standard helper funcs for all our flightish apps
